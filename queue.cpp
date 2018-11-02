9fcf0d6c086da11d92e6a224808e1dcf7dd5af55
Gaku-tohoku <xueyueshanghai@hotmail.com>
2018-11-02 16:42:15 +0800

    Add files via upload

---
+++ queue.cpp
@@ -0,0 +1,22 @@
+
+int f = 0, r = 0, sz = 512;     //f入队游标，r出队游标，sz队大小
+int queue[sz];                  //队存储空间
+
+void enqueue(int item)          //环形队列，当数组游标到达sz后，游标变为0
+{
+    f %= sz;
+    queue[f++] = item;
+}
+int dequeue(void)
+{
+    r %= sz;
+    return queue[r++];
+}
+int is_empty(void)
+{
+    return f == r;
+}
+int is_full(void)
+{
+    return (r+1)%sz == f; 
+}
